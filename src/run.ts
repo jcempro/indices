a5b79bb05113b6f54223cd660884f190d14d2ec7
JeanCarloEM <jeancarlo@jeancarloem.com>
2025-07-07 18:53:32 -0300

    export em run.ts

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,20 @@
-import { ECONIDX } from './engine/EconomicIndicesClient.js';
-import { clearStorage } from './engine/storage.js';
+import {
+	ECONIDX,
+	EconomicIndicesClient,
+} from './engine/EconomicIndicesClient.js';
+import {
+	clearStorage,
+	deveriaAtualizar,
+	loadFromStorage,
+} from './engine/storage.js';
 import { isNodeEnvironment } from './engine/utils.js';
+
+export {
+	EconomicIndicesClient,
+	ECONIDX,
+	deveriaAtualizar,
+	loadFromStorage,
+};
 
 if (typeof window !== 'undefined') {
 	(window as any).ECONIDX = ECONIDX;
